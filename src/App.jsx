254dd8b9aa9220b1d01a7c43b7e472a5eb60aa8a
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-06-11 00:03:30 +0500

    Added App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import {Route, Routes} from 'react-router-dom'
 
-import Sms from './pages/Sms'
-import Devices from './pages/Devices'
-import Company from './pages/Company'
-import DeviceStatus from './pages/DeviceStatus'
-import Version from './pages/Version'
-
 import './App.css'
 import SignIn from './components/SignIn/SignIn';
-import AddNewDevice from './pages/AddNewDevice'
-import AddNewCompany from "./pages/AddNewCompany";
 import RootLayout from './layout/RootLayout'
-import Analysis from './pages/Analysis/Analysis'
 import {useState} from "react";
+
+import {
+	Sms,
+	Device,
+	AddNewDevice,
+	DeviceDetail,
+	DeviceStatus,
+	Company,
+	AddNewCompany,
+	CompanyDetail,
+	Version,
+	Analysis,
+	ZReport,
+	Orders
+} from './pages'
 
 function getUser() {
     let user = localStorage.getItem('user');
@@ -20,7 +26,6 @@ function getUser() {
         user = JSON.parse(user);
     } else {
         user = null;
-
     }
     return user;
 }
@@ -29,28 +34,32 @@ function App() {
     const [user, setUser] = useState(getUser())
 
     return (
-        <div className='App'>
-            {user ? (
-                <Routes>
-                    <Route element={<RootLayout/>}>
-                        <Route path='/analysis' element={<Analysis/>}/>
-                        <Route path='/company' element={<Company/>}/>
-                        <Route path='/devices' element={<Devices/>}/>
-                        <Route path='/devices/status' element={<DeviceStatus/>}/>
-                        <Route path='/create_device' element={<AddNewDevice/>}/>
-                        <Route path='/create_company' element={<AddNewCompany/>}/>
-                        <Route path='/version' element={<Version/>}/>
-                        <Route path='/sms' element={<Sms/>}/>
-                    </Route>
-                    <Route path='/' element={<SignIn/>}/>
-                </Routes>
-            ) : (
-                <Routes>
-                    <Route path='/' element={<SignIn/>}/>
-                </Routes>
-            )}
-        </div>
-    )
+			<div className='App'>
+				{user ? (
+					<Routes>
+						<Route element={<RootLayout />}>
+							<Route path='/analysis' element={<Analysis />} />
+							<Route path='/company' element={<Company />} />
+							<Route path='/company/detail' element={<CompanyDetail />} />
+							<Route path='/device' element={<Device />} />
+							<Route path='/device/detail/:id' element={<DeviceDetail />} />
+							<Route path='/devices/status' element={<DeviceStatus />} />
+							<Route path='/create_device' element={<AddNewDevice />} />
+							<Route path='/create_company' element={<AddNewCompany />} />
+							<Route path='/version' element={<Version />} />
+							<Route path='/sms' element={<Sms />} />
+							<Route path='/z-reports' element={<ZReport />} />
+							<Route path='/orders' element={<Orders />} />
+						</Route>
+						<Route path='/' element={<SignIn />} />
+					</Routes>
+				) : (
+					<Routes>
+						<Route path='/' element={<SignIn />} />
+					</Routes>
+				)}
+			</div>
+		)
 }
 
 export default App;
