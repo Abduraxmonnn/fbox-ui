b6d4004405f2b4ad61dc01c8b609096ec1518600
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-07-22 21:53:58 +0500

    Integrated Search Component to Project.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,81 +1,69 @@
 import {useState, useEffect} from 'react';
 import {Avatar, Spin} from 'antd';
-import {UserOutlined, NotificationOutlined} from '@ant-design/icons'
-import {useDispatch} from "react-redux";
+import {UserOutlined, NotificationOutlined} from '@ant-design/icons';
+import {useDispatch} from 'react-redux';
+import {Link, useNavigate} from 'react-router-dom';
 
 import {images} from '../../constants';
-import {logout} from "../../store/auth/user.action";
+import {logout} from '../../store/auth/user.action';
+import SearchComponent from '../Search/Search';
 import './Header.scss';
-import {Link, useNavigate} from "react-router-dom";
 
-const Header = (props) => {
+const Header = ({isCollapse, searchText, setSearchText}) => {
     const [isUserOptions, setIsUserOptions] = useState(false)
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [userData, setUserData] = useState({});
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const [userData, setUserData] = useState({})
 
     useEffect(() => {
-        const items = JSON.parse(localStorage.getItem('user'));
+        const items = JSON.parse(localStorage.getItem('user'))
         if (items) {
-            setUserData(items);
+            setUserData(items)
         }
-    }, [userData.token]);
+    }, [userData.token])
 
     const handleLogout = () => {
-        dispatch(logout(navigate));
+        dispatch(logout(navigate))
     };
 
-    useEffect(() => {
-        dispatch(logout(navigate));
-    }, [dispatch]);
-
     if (!userData.token) {
-        return <Spin size="large"/>;
+        return <Spin size="large"/>
     }
 
     return (
-        <section className={`header${props.isCollapse ? ' close' : ''}`}>
-            <a href="/analysis"><img src={images.logo} alt='logo'/></a>
-            <div className='search'>
-                <input
-                    type='text'
-                    id='search'
-                    placeholder='Search...'
-                    className='search_input'
-                />
+        <section className={`header${isCollapse ? ' close' : ''}`}>
+            <a href="/analysis"><img src={images.logo} alt="logo"/></a>
+            <div className="search">
+                <SearchComponent searchText={searchText} setSearchText={setSearchText}/>
             </div>
-            <div className='user_info'>
-                <NotificationOutlined className='header_notification'/>
-                <div className='header_user_data'>
+            <div className="user_info">
+                <NotificationOutlined className="header_notification"/>
+                <div className="header_user_data">
                     <span>{userData.data.username}</span>
                     <span>{userData.data.is_superuser ? 'Admin' : 'Client'}</span>
                 </div>
                 <Avatar
                     size={30}
                     icon={<UserOutlined/>}
-                    className='user_avatar'
-                    onClick={() => setIsUserOptions(prev => !prev)}
+                    className="user_avatar"
+                    onClick={() => setIsUserOptions((prev) => !prev)}
                 />
                 {isUserOptions && (
-                    <div
-                        className={`header-menu${
-                            isUserOptions ? ' show-menu' : ''
-                        }`}
-                    >
+                    <div className={`header-menu${isUserOptions ? ' show-menu' : ''}`}>
                         <ul>
                             <li>
-                                <img src={images.manage_account} alt='manage'/>
+                                <img src={images.manage_account} alt="manage"/>
                                 <span>Manage Account</span>
                             </li>
                             <hr/>
                             <li>
-                                <img src={images.activity_log} alt='activity'/>
+                                <img src={images.activity_log} alt="activity"/>
                                 <span>Activity Log</span>
                             </li>
                             <hr/>
                             <Link to="/">
                                 <li onClick={handleLogout} role="button" tabIndex={0}>
-                                    <img src={images.logout} alt='log out'/>
+                                    <img src={images.logout} alt="log out"/>
                                     <span>Log out</span>
                                 </li>
                             </Link>
--- src/layout/SideBar.jsx
+++ src/layout/SideBar.jsx
@@ -1,76 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import { Breadcrumb, Layout, Menu, theme } from 'antd'
-import Header from '../components/Header/Header'
-import { items } from '../constants'
-import { useLocation, NavLink, Outlet } from 'react-router-dom'
+import React, {useState, useEffect} from 'react';
+import {Breadcrumb, Layout, Menu, theme} from 'antd';
+import Header from '../components/Header/Header';
+import {items} from '../constants';
+import {useLocation, NavLink, Outlet} from 'react-router-dom';
 
-const { Content, Footer, Sider } = Layout
+const {Content, Footer, Sider} = Layout;
 
 const SideBar = () => {
-	const [collapsed, setCollapsed] = useState(false)
-	const {
-		token: { colorBgContainer, borderRadiusLG },
-	} = theme.useToken()
-	const [placement, setPlacement] = useState('right')
+    const [collapsed, setCollapsed] = useState(false)
+    const {
+        token: {colorBgContainer, borderRadiusLG},
+    } = theme.useToken()
+    const [placement, setPlacement] = useState('right')
+    const [searchText, setSearchText] = useState('')
 
-	let { pathname } = useLocation()
-	pathname = pathname.replace('/', '')
+    let {pathname} = useLocation();
+    pathname = pathname.replace('/', '')
 
-	useEffect(() => {
-		if (pathname === 'rtl') {
-			setPlacement('left')
-		} else {
-			setPlacement('right')
-		}
-	}, [pathname])
+    useEffect(() => {
+        if (pathname === 'rtl') {
+            setPlacement('left')
+        } else {
+            setPlacement('right')
+        }
+    }, [pathname])
 
-	return (
-		<section className='sidebar'>
-			<Layout
-				style={{
-					minHeight: '100vh',
-				}}
-			>
-				<Sider
-					collapsible
-					collapsed={collapsed}
-					onCollapse={value => setCollapsed(value)}
-					theme='light'
-				>
-					<div className='demo-logo-vertical' />
-					<Menu
-						defaultSelectedKeys={['1']}
-						mode='inline'
-						items={items}
-					/>
-				</Sider>
-				<Layout>
-					<Header isCollapse={collapsed} />
-					{/* <CustomHeader /> */}
-					<Content
-						style={{
-							margin: '0 16px',
-						}}
-					>
-						<Breadcrumb style={{ margin: '16px 0' }}>
-							<Breadcrumb.Item>
-								<NavLink to='/analysis'>Main</NavLink>
-							</Breadcrumb.Item>
-							<Breadcrumb.Item
-								style={{ textTransform: 'capitalize' }}
-							>
-								{pathname.replace('/', '')}
-							</Breadcrumb.Item>
-						</Breadcrumb>
-						<Outlet />
-					</Content>
-					<Footer style={{ textAlign: 'center' }}>
-						BCTechnologies ©{new Date().getFullYear()}
-					</Footer>
-				</Layout>
-			</Layout>
-		</section>
-	)
+    return (
+        <section className='sidebar'>
+            <Layout style={{minHeight: '100vh'}}>
+                <Sider
+                    collapsible
+                    collapsed={collapsed}
+                    onCollapse={(value) => setCollapsed(value)}
+                    theme='light'
+                >
+                    <div className='demo-logo-vertical'/>
+                    <Menu defaultSelectedKeys={['1']} mode='inline' items={items}/>
+                </Sider>
+                <Layout>
+                    <Header isCollapse={collapsed} searchText={searchText} setSearchText={setSearchText}/>
+                    <Content style={{margin: '0 16px'}}>
+                        <Breadcrumb style={{margin: '16px 0'}}>
+                            <Breadcrumb.Item>
+                                <NavLink to='/analysis'>Main</NavLink>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item style={{textTransform: 'capitalize'}}>
+                                {pathname.replace('/', '')}
+                            </Breadcrumb.Item>
+                        </Breadcrumb>
+                        <Outlet context={{searchText, setSearchText}}/>
+                    </Content>
+                    <Footer style={{textAlign: 'center'}}>
+                        BCTechnologies ©{new Date().getFullYear()}
+                    </Footer>
+                </Layout>
+            </Layout>
+        </section>
+    )
 }
 
-export default SideBar
+export default SideBar;
