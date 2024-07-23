186c67a671d6f9d7dc69e58ca0873d396febe669
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-07-23 22:07:42 +0500

    Added Search and Ordering on SUbscription Page

--- src/pages/Subscriptions/Subscription.jsx
+++ src/pages/Subscriptions/Subscription.jsx
@@ -1,19 +1,16 @@
-import React, {useState, useEffect} from 'react'
-import {Table, Tag, FloatButton} from 'antd'
-import {
-    FileAddOutlined,
-    FileExcelOutlined,
-    UploadOutlined,
-} from '@ant-design/icons'
-import {APIv1} from '../../api'
-import {Link, useOutletContext} from 'react-router-dom'
+import React, {useState, useEffect, useCallback} from 'react';
+import {Table, Tag, FloatButton} from 'antd';
+import {FileAddOutlined, FileExcelOutlined, UploadOutlined} from '@ant-design/icons';
+import {APIv1} from '../../api';
+import {Link, useOutletContext} from 'react-router-dom';
+import {handleTableChange} from "../../utils";
 
-const columns = (searchText) => [
+const columns = [
     {
         title: 'Subscription serial number',
         dataIndex: 'device_serial_number',
-        sorter: (a, b) =>
-            a.device_serial_number.localeCompare(b.device_serial_number),
+        sorter: true,
+        orderIndex: "device_serial_number",
         onFilter: (value, record) => record.device_serial_number.startsWith(value),
         render: (text, record) => (
             <Link to={`/subscription/detail/${record.key}`}>{text}</Link>
@@ -52,16 +49,12 @@ const columns = (searchText) => [
             const today = new Date().toISOString().slice(0, 10)
             const pastSevenDays = new Date(
                 new Date().setDate(new Date().getDate() - 7)
-            )
-                .toISOString()
-                .slice(0, 10)
+            ).toISOString().slice(0, 10)
             switch (value) {
                 case 'today':
                     return record.start_date === today
                 case 'pastSevenDays':
-                    return (
-                        record.start_date >= pastSevenDays && record.start_date <= today
-                    )
+                    return record.start_date >= pastSevenDays && record.start_date <= today
                 case 'noDate':
                     return record.start_date === '----/--/--'
                 case 'hasDate':
@@ -85,9 +78,7 @@ const columns = (searchText) => [
             const today = new Date().toISOString().slice(0, 10)
             const pastSevenDays = new Date(
                 new Date().setDate(new Date().getDate() - 7)
-            )
-                .toISOString()
-                .slice(0, 10)
+            ).toISOString().slice(0, 10)
             switch (value) {
                 case 'today':
                     return record.end_date === today
@@ -102,63 +93,92 @@ const columns = (searchText) => [
             }
         },
     },
-]
+];
+
+const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+        console.log(
+            `selectedRowKeys: ${selectedRowKeys}`,
+            'selectedRows: ',
+            selectedRows
+        );
+    },
+};
 
 const Subscription = () => {
-    let defaultPageSize = 20
+    const defaultPageSize = 20;
     const [subscriptionsData, setSubscriptionsData] = useState([])
     const [selectionType, setSelectionType] = useState('checkbox')
     const [loading, setLoading] = useState(true)
     const [totalSubscriptions, setTotalSubscriptions] = useState(0)
     const [currentPage, setCurrentPage] = useState(1)
     const [pageSize, setPageSize] = useState(defaultPageSize)
-    const {searchText} = useOutletContext();
+    const [sortField, setSortField] = useState('')
+    const [sortOrder, setSortOrder] = useState('')
+    const {searchText} = useOutletContext()
 
-    useEffect(() => {
-        getSubscriptionData(currentPage, pageSize);
-    }, [currentPage, pageSize])
-
-    const onChange = (page, pageSize) => {
-        setCurrentPage(page)
-        setPageSize(pageSize)
-    }
-
-    const extractDate = (dateString) => {
-        const date = new Date(dateString)
-        return date.toISOString().slice(0, 10)
-    };
-
-    async function getSubscriptionData(page, size) {
+    const fetchSubscriptions = useCallback(async (page, size, search = '', ordering = '') => {
         setLoading(true);
         try {
-            const response = await APIv1.get(`/devices?page=${page}&page_size=${size}`);
-            const devicesData = response.data.results.map((subscription) => ({
+            const response = await APIv1.get(`/devices`, {
+                params: {
+                    page,
+                    page_size: size,
+                    search,
+                    ordering,
+                }
+            })
+            const data = response.data.results.map((subscription) => ({
                 key: subscription.id,
                 device_serial_number: subscription.device_serial_number,
                 is_multi_user: [subscription.is_multi_user],
                 start_date: subscription.start_date ? extractDate(subscription.start_date) : '----/--/--',
                 end_date: subscription.end_date ? extractDate(subscription.end_date) : '----/--/--',
             }));
-            setSubscriptionsData(devicesData)
+            setSubscriptionsData(data)
             setTotalSubscriptions(response.data.count)
         } catch (err) {
             console.error('Something went wrong:', err)
         } finally {
-            setLoading(false);
+            setLoading(false)
         }
-    }
+    }, []);
 
-    const filteredDevices = subscriptionsData.filter((subscription) =>
-        subscription.device_serial_number.toLowerCase().includes(searchText.toLowerCase())
-    );
+    useEffect(() => {
+        let ordering = ''
+        if (sortField) {
+            ordering = sortOrder === 'ascend' ? sortField : `-${sortField}`
+        }
+        fetchSubscriptions(currentPage, pageSize, searchText, ordering)
+    }, [currentPage, pageSize, searchText, sortOrder, sortField, fetchSubscriptions])
+
+    useEffect(() => {
+        setCurrentPage(1) // Reset to the first page when search text changes
+    }, [searchText])
+
+    const onChange = (page, pageSize) => {
+        setCurrentPage(page)
+        setPageSize(pageSize)
+    };
+
+    const tableChangeHandler = handleTableChange(setSortField, setSortOrder, columns);
+
+    const extractDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toISOString().slice(0, 10);
+    };
 
     return (
         <div className='content_container'>
             <Table
-                rowSelection={{type: selectionType}}
-                columns={columns(searchText)}
-                dataSource={filteredDevices}
+                rowSelection={{
+                    type: selectionType,
+                    ...rowSelection,
+                }}
+                columns={columns}
+                dataSource={subscriptionsData}
                 loading={loading}
+                onChange={tableChangeHandler}
                 pagination={{
                     total: totalSubscriptions,
                     current: currentPage,
