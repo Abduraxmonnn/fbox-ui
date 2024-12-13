249b5e15459fac01931b9ac75a418de0ca1cc8c2
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-12-13 14:46:20 +0500

    Updated/Added: Analysis component

--- src/pages/Analysis/Analysis.jsx
+++ src/pages/Analysis/Analysis.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-
-import PieChart from '../../components/AnalysisStats/PieChart'
-import BarChart from '../../components/AnalysisStats/BarChart'
+import React from 'react';
 import {useNavigate} from "react-router-dom";
+import PieChart from '../../components/AnalysisStats/PieChart';
+import BarChart from '../../components/AnalysisStats/BarChart';
 import Orders from "../../components/Analysis/Orders";
-import TransactionFinancialCard from "../../components/AnalysisStats/TransactionFinancial/TransactionFinancialCard";
-import Period from "../../components/AnalysisStats/Period";
-import TransactionCountCard from "../../components/AnalysisStats/TransactionCount/TransactionCountCard";
-import './Analysis.css'
+import TransactionFinancialCard from '../../components/AnalysisStats/TransactionFinancial/TransactionFinancialCard';
+import Period from '../../components/AnalysisStats/Period';
+import TransactionCountCard from '../../components/AnalysisStats/TransactionCount/TransactionCountCard';
+import './Analysis.css';
 
 const Analysis = () => {
     const navigate = useNavigate();
@@ -17,36 +16,48 @@ const Analysis = () => {
     };
 
     return (
-        <section className='content_container'>
-            <div className='box'>
+        <div className='analysis'>
+            <div className='analysis__header'>
                 <Period/>
             </div>
-            <div className="upper-container">
-                <div className='box'>
+
+            <div className="analysis__metrics">
+                <div className='analysis__metric-card'>
                     <TransactionFinancialCard/>
                 </div>
-                <div className='box'>
+                <div className='analysis__metric-card'>
                     <TransactionCountCard/>
                 </div>
             </div>
-            <div className='graphBox'>
-                <div className='box'>
-                    <h1>Active Devices</h1>
-                    <PieChart/>
+
+            <div className='analysis__charts'>
+                <div className='analysis__chart-card'>
+                    <h2 className='analysis__chart-title'>Active Devices</h2>
+                    <div className='analysis__chart-content'>
+                        <PieChart/>
+                    </div>
                 </div>
-                <div className='box'>
-                    <h1>Completed Payments</h1>
-                    <BarChart/>
+                <div className='analysis__chart-card analysis__chart-card--wide'>
+                    <h2 className='analysis__chart-title'>Completed Payments</h2>
+                    <div className='analysis__chart-content'>
+                        <BarChart/>
+                    </div>
                 </div>
             </div>
-            <div className='content'>
-                <div className='orders'>
-                    <h1 onClick={() => handleNavigate("/orders")}>Orders</h1>
+
+            <div className='analysis__orders'>
+                <div className='analysis__orders-card'>
+                    <h2
+                        className='analysis__orders-title'
+                        onClick={() => handleNavigate("/orders")}
+                    >
+                        Orders
+                    </h2>
                     <Orders/>
                 </div>
             </div>
-        </section>
-    )
-}
+        </div>
+    );
+};
 
 export default Analysis;
