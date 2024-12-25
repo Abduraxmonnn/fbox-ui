28393db39e74afb4c2857754e5f9c79cc02fd00e
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-12-25 14:04:11 +0500

    Modified: Active Device Count Card

--- src/components/AnalysisStats/NumberCards/DeviceStatus/DeviceStatusCard.jsx
+++ src/components/AnalysisStats/NumberCards/DeviceStatus/DeviceStatusCard.jsx
@@ -2,16 +2,51 @@ import CountUp from "react-countup";
 import {MonitorCheck, MonitorDot} from 'lucide-react';
 import '../BaseNumberCardStyle.css'
 import './DeviceStatusCard.css'
+import {useCallback, useEffect, useState} from "react";
+import {APIv1} from "../../../../api";
 
 interface TransactionCountCardProps {
     successAmount?: number;
     failureAmount?: number;
 }
 
-const DeviceStatusCard: React.FC<TransactionCountCardProps> = ({
-                                                                       activeCount = 45,
-                                                                       inactiveCount = 27
-                                                                   }) => {
+const DeviceStatusCard: React.FC<TransactionCountCardProps> = () => {
+    const [userData, setUserData] = useState({});
+    const [data, setData] = useState([]);
+
+    const fetchUserData = useCallback(async () => {
+        try {
+            const response = await APIv1.get('/device/status/', {
+                headers: {
+                    Authorization: `Token ${userData.token}`,
+                },
+            });
+            const responseData = response.data;
+
+            setData({
+                activeCount: responseData.online,
+                inactiveCount: responseData.offline,
+            });
+
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    }, [userData.token]);
+
+    useEffect(() => {
+        if (!userData.token) return;
+
+        fetchUserData()
+    }, [userData.token])
+
+
+    useEffect(() => {
+        const items = JSON.parse(localStorage.getItem('user'));
+        if (items) {
+            setUserData(items);
+        }
+    }, [userData.token]);
+
     return (
         <div className="transaction-metrics">
             <h2 className="transaction-metrics__title">Active Devices</h2>
@@ -22,7 +57,7 @@ const DeviceStatusCard: React.FC<TransactionCountCardProps> = ({
                     <div className="transaction-metrics__count">
                         <MonitorCheck className="transaction-metrics__icon transaction-metrics__icon--active"/>
                         <CountUp
-                            end={activeCount}
+                            end={data.activeCount}
                             duration={3}
                         />
                     </div>
@@ -33,7 +68,7 @@ const DeviceStatusCard: React.FC<TransactionCountCardProps> = ({
                     <div className="transaction-metrics__count">
                         <MonitorDot className="transaction-metrics__icon transaction-metrics__icon--inactive"/>
                         <CountUp
-                            end={inactiveCount}
+                            end={data.inactiveCount}
                             duration={3}
                         />
                     </div>
