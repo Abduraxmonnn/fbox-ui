74ec324c026dd6066d77385a58ae1bcdee1948f4
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-01-02 10:31:06 +0500

    added(pages.orders): localization

---
+++ src/pages/Orders/orders.constants.js
@@ -0,0 +1,53 @@
+import {Link} from "react-router-dom";
+import {useTranslation} from 'react-i18next';
+
+const OrdersColumns = () => {
+    const {t} = useTranslation();
+
+    return [
+        {
+            title: t('pages.orders.listColumns.column1'),
+            dataIndex: 'market_name',
+            render: (text, record) => (
+                <Link to={`/order/detail/${record.key}`}>{text}</Link>
+            ),
+            sorter: true,
+            orderIndex: "market_name",
+        },
+        {
+            title: t('pages.orders.listColumns.column2'),
+            dataIndex: 'cash_desc_serial',
+            render: (text, record) => (
+                <Link to={`/order/detail/${record.key}`}>{text}</Link>
+            ),
+            sorter: true,
+            orderIndex: "cash_desc_serial",
+        },
+        {
+            title: t('pages.orders.listColumns.column3'),
+            dataIndex: 'cashier',
+            sorter: true,
+            orderIndex: "cashier",
+        },
+        {
+            title: t('pages.orders.listColumns.column4'),
+            dataIndex: 'received_cash',
+            sorter: true,
+            orderIndex: "received_cash",
+        },
+        {
+            title: t('pages.orders.listColumns.column5'),
+            dataIndex: 'received_card',
+            sorter: true,
+            orderIndex: "received_card",
+        },
+        {
+            title: t('pages.orders.listColumns.column6'),
+            dataIndex: 'time',
+            sorter: true,
+            orderIndex: "time",
+        },
+    ]
+};
+
+export default OrdersColumns;
