f9dd40dbaff3428c9185aaf526653a9ac6a7354c
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-01-03 23:07:17 +0500

    feat(save-localization-option): save selected lang on reloading page

--- src/components/Localizations/Localization.jsx
+++ src/components/Localizations/Localization.jsx
@@ -1,17 +1,20 @@
 import {useTranslation} from 'react-i18next';
 import {Select, Space} from 'antd';
+import {useEffect} from "react";
 
 const Localization = () => {
     const {i18n} = useTranslation();
 
     function changeLanguage(value) {
-        i18n.changeLanguage(value); // Use the selected value directly
+        i18n.changeLanguage(value);
+        localStorage.setItem('i18nextLng', value);
     }
 
     return (
         <>
             <Space wrap>
                 <Select
+                    value={i18n.language}
                     defaultValue="en"
                     style={{
                         width: 130,
--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,23 +1,34 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
+import Backend from 'i18next-localstorage-backend';
 
-i18n.use(initReactI18next).init({
-    fallbackLng: 'en',
-    lng: 'en',
-    resources: {
-        en: {
-            translations: require('./locales/en.json')
+i18n
+    .use(Backend)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        lng: localStorage.getItem('i18nextLng') || 'en',
+        resources: {
+            en: {
+                translations: require('./locales/en.json')
+            },
+            ru: {
+                translations: require('./locales/ru.json')
+            },
+            uz: {
+                translations: require('./locales/uz.json')
+            }
         },
-        ru: {
-            translations: require('./locales/ru.json')
+        ns: ['translations'],
+        defaultNS: 'translations',
+        detection: {
+            order: ['localStorage', 'navigator'],
+            caches: ['localStorage'],
         },
-        uz: {
-            translations: require('./locales/uz.json')
+        react: {
+            useSuspense: false,
         }
-    },
-    ns: ['translations'],
-    defaultNS: 'translations'
-});
+    });
 
 i18n.languages = ['en', 'ru', 'uz'];
 
