363d1552e71bd85708436f70587c6b3664c5e038
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-01-09 12:58:03 +0500

    feat(icons): replace christmas icons to normal

--- src/constants/SideBarItems.js
+++ src/constants/SideBarItems.js
@@ -1,25 +1,15 @@
-// import {
-//     PieChartOutlined,
-//     MessageOutlined,
-//     CheckCircleOutlined,
-//     ClusterOutlined,
-//     FileDoneOutlined,
-//     CreditCardOutlined,
-//     HistoryOutlined
-// } from '@ant-design/icons'
 import {
-    IoGiftOutline,
-    IoSnowOutline,
-    IoCloudOutline,
-    IoStarOutline,
-    IoHomeOutline,
-    IoPeopleOutline,
-    IoMoonOutline,
-    IoCalendarOutline
-} from 'react-icons/io5';
+    PieChartOutlined,
+    MessageOutlined,
+    ClusterOutlined,
+    FileDoneOutlined,
+    CreditCardOutlined,
+    HistoryOutlined,
+    MailOutlined,
+    DesktopOutlined
+} from '@ant-design/icons'
 import {Link} from 'react-router-dom'
 import {Landmark} from 'lucide-react';
-import {MailOutlined} from "@ant-design/icons";
 
 export function getItem(label, key, icon, children) {
     return {
@@ -36,42 +26,36 @@ const SideBarItems = (t) => {
             t('sideBar.items.column1'),
             'analysis_item',
             <Link to='/analysis'>
-                {/*<PieChartOutlined/>*/}
-                <IoSnowOutline/>
+                <PieChartOutlined/>
             </Link>
         ),
         getItem(
             t('sideBar.items.column2'),
             'devices_company_item',
             <Link to='/devices/company'>
-                {/*<Landmark size={14}/>*/}
-                <IoHomeOutline/>
+                <Landmark size={14}/>
             </Link>
         ),
         getItem(
             t('sideBar.items.column3'),
             'devices_status_item',
             <Link to='/devices'>
-                {/*<CheckCircleOutlined/>*/}
-                <IoStarOutline/>
+                <DesktopOutlined/>
             </Link>
         ),
-        // <HistoryOutlined/>
-        getItem(t('sideBar.items.column4'), 'payments_items', <IoCalendarOutline/>, [
+        getItem(t('sideBar.items.column4'), 'payments_items', <HistoryOutlined/>, [
             getItem(
                 t('sideBar.items.column5'),
                 'log_payments_items',
                 <Link to='/payments/logs'>
-                    {/*<CreditCardOutlined/>*/}
-                    <IoMoonOutline/>
+                    <CreditCardOutlined/>
                 </Link>
             ),
             getItem(
                 t('sideBar.items.column6'),
                 'sms_payments_items',
                 <Link to='/payments/sms'>
-                    {/*<MessageOutlined/>*/}
-                    <IoPeopleOutline/>
+                    <MessageOutlined/>
                 </Link>
             ),
             getItem(
@@ -86,16 +70,14 @@ const SideBarItems = (t) => {
             t('sideBar.items.column8'),
             'orders_orders_item',
             <Link to='/orders'>
-                {/*<FileDoneOutlined/>*/}
-                <IoGiftOutline/>
+                <FileDoneOutlined/>
             </Link>
         ),
         getItem(
             t('sideBar.items.column9'),
             'devices_versions_item',
             <Link to='/devices/version'>
-                {/*<ClusterOutlined/>*/}
-                <IoCloudOutline/>
+                <ClusterOutlined/>
             </Link>
         ),
         // getItem(
