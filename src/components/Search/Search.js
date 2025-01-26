2b95121be443ef2a43f085da23f3e23b13cc7355
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-01-26 15:42:35 +0500

    feat(prefix): added prefix to search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
-import { Input } from 'antd';
-import { useLocation } from 'react-router-dom'; // Import useLocation
+import React, {useEffect} from 'react';
+import {Input, Tooltip} from 'antd';
+import {useLocation} from 'react-router-dom'; // Import useLocation
 import './Search.scss';
+import {InfoCircleOutlined, SyncOutlined} from "@ant-design/icons";
 
-const { Search } = Input;
+const {Search} = Input;
 
-const SearchComponent = ({ t, searchText, setSearchText }) => {
+const SearchComponent = ({t, searchText, setSearchText}) => {
     const location = useLocation(); // Track route changes
 
     useEffect(() => {
@@ -19,12 +20,21 @@ const SearchComponent = ({ t, searchText, setSearchText }) => {
     return (
         <div className="search">
             <Search
+                prefix={
+                    <Tooltip title="Type anything to search with system">
+                        <InfoCircleOutlined
+                            style={{
+                                color: 'rgba(0,0,0,.45)',
+                            }}
+                        />
+                    </Tooltip>
+                }
                 allowClear={true}
                 showCount={true}
                 placeholder={t("header.placeholder1")}
                 value={searchText}
                 onChange={handleSearch}
-                style={{ width: 500 }}
+                style={{width: 500}}
             />
         </div>
     );
