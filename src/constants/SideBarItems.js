05c7b7621af5eca32d8397c080dad3984236aa83
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-01-31 23:21:42 +0500

    feat(side-bar): change side bar items for clients

--- src/constants/SideBarItems.js
+++ src/constants/SideBarItems.js
@@ -10,6 +10,7 @@ import {
 } from '@ant-design/icons'
 import {Link} from 'react-router-dom'
 import {Landmark} from 'lucide-react';
+import {useEffect, useState} from "react";
 
 export function getItem(label, key, icon, children) {
     return {
@@ -21,19 +22,21 @@ export function getItem(label, key, icon, children) {
 }
 
 const SideBarItems = (t) => {
-    return [
+    const [isUserStaff, setIsUserStaff] = useState({});
+
+    useEffect(() => {
+        const items = JSON.parse(localStorage.getItem('user'));
+        if (items) {
+            setIsUserStaff(items.data.is_staff)
+        }
+    }, [isUserStaff]);
+
+    const clientItems = [
         getItem(
             t('sideBar.items.column1'),
             'analysis_item',
             <Link to='/analysis'>
                 <PieChartOutlined/>
-            </Link>
-        ),
-        getItem(
-            t('sideBar.items.column2'),
-            'devices_company_item',
-            <Link to='/devices/company'>
-                <Landmark size={14}/>
             </Link>
         ),
         getItem(
@@ -43,53 +46,96 @@ const SideBarItems = (t) => {
                 <DesktopOutlined/>
             </Link>
         ),
-        getItem(t('sideBar.items.column4'), 'payments_items', <HistoryOutlined/>, [
-            getItem(
-                t('sideBar.items.column5'),
-                'log_payments_items',
-                <Link to='/payments/logs'>
-                    <CreditCardOutlined/>
-                </Link>
-            ),
-            getItem(
-                t('sideBar.items.column6'),
-                'sms_payments_items',
-                <Link to='/payments/sms'>
-                    <MessageOutlined/>
-                </Link>
-            ),
-            getItem(
-                t('sideBar.items.column7'),
-                'email_payments_items',
-                <Link to='/email/list'>
-                    <MailOutlined/>
-                </Link>
-            ),
-        ]),
         getItem(
-            t('sideBar.items.column8'),
-            'orders_orders_item',
-            <Link to='/orders'>
-                <FileDoneOutlined/>
+            t('sideBar.items.column5'),
+            'log_payments_items',
+            <Link to='/payments/logs'>
+                <CreditCardOutlined/>
             </Link>
         ),
         getItem(
-            t('sideBar.items.column9'),
-            'devices_versions_item',
-            <Link to='/devices/version'>
-                <ClusterOutlined/>
+            t('sideBar.items.column6'),
+            'sms_payments_items',
+            <Link to='/payments/sms'>
+                <MessageOutlined/>
             </Link>
         ),
-        // getItem(
-        // 	'Z-Reports',
-        // 	'zreports_item',
-        // 	<Link to='/z-reports'>
-        // 		<AlertOutlined />
-        // 	</Link>
-        // ),
-        // getItem('Equipments', 'equipments_equipment_item', <ApiOutlined />),
-        // getItem('Pricing', 'pricing_pricing_item', <DollarOutlined />),
+        getItem(
+            t('sideBar.items.column7'),
+            'email_payments_items',
+            <Link to='/email/list'>
+                <MailOutlined/>
+            </Link>
+        ),
     ]
+
+
+    return (
+        isUserStaff ? (
+            [
+                getItem(
+                    t('sideBar.items.column1'),
+                    'analysis_item',
+                    <Link to='/analysis'>
+                        <PieChartOutlined/>
+                    </Link>
+                ),
+                getItem(
+                    t('sideBar.items.column2'),
+                    'devices_company_item',
+                    <Link to='/devices/company'>
+                        <Landmark size={14}/>
+                    </Link>
+                ),
+                getItem(
+                    t('sideBar.items.column3'),
+                    'devices_status_item',
+                    <Link to='/devices'>
+                        <DesktopOutlined/>
+                    </Link>
+                ),
+                getItem(t('sideBar.items.column4'), 'payments_items', <HistoryOutlined/>, [
+                    getItem(
+                        t('sideBar.items.column5'),
+                        'log_payments_items',
+                        <Link to='/payments/logs'>
+                            <CreditCardOutlined/>
+                        </Link>
+                    ),
+                    getItem(
+                        t('sideBar.items.column6'),
+                        'sms_payments_items',
+                        <Link to='/payments/sms'>
+                            <MessageOutlined/>
+                        </Link>
+                    ),
+                    getItem(
+                        t('sideBar.items.column7'),
+                        'email_payments_items',
+                        <Link to='/email/list'>
+                            <MailOutlined/>
+                        </Link>
+                    ),
+                ]),
+                getItem(
+                    t('sideBar.items.column8'),
+                    'orders_orders_item',
+                    <Link to='/orders'>
+                        <FileDoneOutlined/>
+                    </Link>
+                ),
+                getItem(
+                    t('sideBar.items.column9'),
+                    'devices_versions_item',
+                    <Link to='/devices/version'>
+                        <ClusterOutlined/>
+                    </Link>
+                ),
+            ]
+        ) : (
+            clientItems
+        )
+    )
 }
 
 export default SideBarItems;
