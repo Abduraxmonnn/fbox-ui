b2b2d4e56ad9b7edc1a83f502dbaae1c0e5e00f2
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-02-05 10:05:02 +0500

    removed(snow fall):

--- src/index.js
+++ src/index.js
@@ -7,17 +7,12 @@ import './i18n/config';
 import './index.css';
 import App from './App';
 import {store} from './store/store';
-import {Snowfall} from 'react-snowfall';
 
 ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
             <BrowserRouter>
                 <App/>
-                <Snowfall
-                    snowflakeCount={200}
-                    style={{position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 10}}
-                />
             </BrowserRouter>
         </React.StrictMode>
     </Provider>,
