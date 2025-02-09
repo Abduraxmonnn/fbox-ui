c3e60a04fdcfa8a0c28d2b1237d9dbdcf07c7530
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2025-02-09 21:33:07 +0500

    modified(product): added new fields and re-design

--- src/pages/Products/ProductDetail/ProductDetail.jsx
+++ src/pages/Products/ProductDetail/ProductDetail.jsx
@@ -3,7 +3,6 @@ import React, {useEffect, useState} from "react";
 import {useTranslation} from "react-i18next";
 import {APIv1} from "../../../api";
 import CurrencyFormatted from "../../../utils/baseFormatter";
-import Products from "../../Products/Products";
 import '../../../styles/BaseDetailStyle.scss'
 
 const ProductDetail = () => {
@@ -12,12 +11,6 @@ const ProductDetail = () => {
     const [productData, setProductData] = useState(null);
     const [userData, setUserData] = useState({});
     const navigate = useNavigate();
-
-    const extractDate = (dateString) => {
-        if (!dateString) return '-'; // Return a placeholder if no date is available
-        const date = new Date(dateString);
-        return isNaN(date) ? '-' : date.toISOString().slice(0, 10); // Check if the date is invalid
-    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -54,9 +47,9 @@ const ProductDetail = () => {
                 <div className="detail-view__title">
                     <h1 className="detail-view__main-title">{productData.name}</h1>
                     <span className="detail-view__subtitle">
-              <span className="detail-view__subtitle-label">{t("common.deviceSerial")}: </span>
-                        {productData.barcode}
-            </span>
+                    <span className="detail-view__subtitle-label">{t("common.inn")}: </span>
+                        {productData.order.inn}
+                    </span>
                 </div>
                 <button
                     className="detail-view__action-button detail-view__action-button--secondary"
@@ -68,42 +61,42 @@ const ProductDetail = () => {
 
             <div className="detail-view__content">
                 <div className="detail-view__section">
-                    <h2 className="detail-view__section-title">{t("pages.products.title")}</h2>
                     <ul className="detail-view__list">
-                        {[
-                            {
-                                label: `${t("pages.products.detailColumns.showcase1")}`,
-                                value: productData.name
-                            },
-                            {label: `${t("pages.products.detailColumns.showcase2")}`, value: productData.barcode},
-                            {
-                                label: `${t("pages.products.detailColumns.showcase3")}`,
-                                value: CurrencyFormatted(productData.amount)
-                            },
-                            {
-                                label: `${t("pages.products.detailColumns.showcase4")}`,
-                                value: CurrencyFormatted(productData.product_price)
-                            },
-                            {label: `${t("pages.products.detailColumns.showcase5")}`, value: CurrencyFormatted(productData.price)},
-                            {
-                                label: `${t("pages.products.detailColumns.showcase6")}`,
-                                value: productData.discount
-                            },
-                            {
-                                label: `${t("pages.products.detailColumns.showcase7")}`,
-                                value: productData.discount_percent
-                            },
-                        ].map(({label, value}) => (
-                            <li key={label} className="detail-view__item">
-                                <span className="detail-view__label">{label}:</span>
-                                <span className="detail-view__value">{value}</span>
-                            </li>
-                        ))}
+                        <li className="detail-view__item">
+                            <span className="detail-view__label">{t("pages.products.detailColumns.showcase9")}:</span>
+                            <span className="detail-view__value">{productData.order.market_name}</span>
+                        </li>
+                        <li className="detail-view__item">
+                            <span className="detail-view__label">{t("pages.products.detailColumns.showcase1")}:</span>
+                            <span className="detail-view__value">{productData.name}</span>
+                        </li>
+                        <li className="detail-view__item">
+                            <span className="detail-view__label">{t("pages.products.detailColumns.showcase2")}:</span>
+                            <span className="detail-view__value">{productData.barcode}</span>
+                        </li>
                         <li className="detail-view__item detail-view__item--highlighted">
-                            <span
-                                className="detail-view__label">{t("pages.products.detailColumns.showcase8")}:</span>
-                            <span
-                                className="detail-view__value">{productData.class_code}</span>
+                            <span className="detail-view__label">{t("pages.products.detailColumns.showcase3")}:</span>
+                            <span className="detail-view__value">{CurrencyFormatted(productData.amount)}</span>
+                        </li>
+                        <li className="detail-view__item detail-view__item--highlighted">
+                            <span className="detail-view__label">{t("pages.products.detailColumns.showcase4")}:</span>
+                            <span className="detail-view__value">{CurrencyFormatted(productData.product_price)}</span>
+                        </li>
+                        <li className="detail-view__item detail-view__item--highlighted">
+                            <span className="detail-view__label">{t("pages.products.detailColumns.showcase5")}:</span>
+                            <span className="detail-view__value">{CurrencyFormatted(productData.price)}</span>
+                        </li>
+                        <li className="detail-view__item">
+                            <span className="detail-view__label">{t("pages.products.detailColumns.showcase6")}:</span>
+                            <span className="detail-view__value">{productData.discount}</span>
+                        </li>
+                        <li className="detail-view__item">
+                            <span className="detail-view__label">{t("pages.products.detailColumns.showcase7")}:</span>
+                            <span className="detail-view__value">{productData.discount_percent}</span>
+                        </li>
+                        <li className="detail-view__item">
+                            <span className="detail-view__label">{t("pages.products.detailColumns.showcase8")}:</span>
+                            <span className="detail-view__value">{productData.class_code}</span>
                         </li>
                     </ul>
                 </div>
